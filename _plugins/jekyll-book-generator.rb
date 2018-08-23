a41a08408eb80e2d11e0abf2b17adca946ce2551
zhangshiyu <741448394@qq.com>
2018-08-23 09:31:56 +0800

    移植gitbook生成书籍功能

--- _plugins/jekyll-book-generator.rb
+++ _plugins/jekyll-book-generator.rb
@@ -17,6 +17,40 @@ module Jekyll
     end
   end
 
+  class ChapterPage < Page
+    def initialize(site, base, dir, name, book, config)
+      @site = site
+      @base = base
+
+      if name.include?("/")
+        names = File.split(name)
+        @dir = File.join(dir, names[0])
+        @name = names[1]
+      else
+        @dir = dir
+        @name = name
+      end
+
+      self.process(@name == "README.md" ? "index.html" : @name)
+
+      read_yaml(File.join(base, "_books", @dir), @name)
+
+      self.data["layout"] = 'book'
+      self.data["book"] = book
+
+      if @name == "README.md"
+        self.data["link"] = File.join(@dir, "index.html")
+      else
+        basename = File.basename(@name, '.md')
+        newname = basename + ".html"
+        self.data["link"] = File.join(@dir, newname)
+      end
+
+      self.data["title"] = config["title"] 
+      self.data["level"] = config["level"]
+    end
+  end
+
   class BookGenerator < Generator
     def generate(site)
 
@@ -29,12 +63,13 @@ module Jekyll
         Dir.foreach(dir) do |book_dir|
           book_path = File.join(dir, book_dir)
           if File.directory?(book_path) and book_dir.chars.first != "."
+
+            # 解析书籍配置
             config_file = File.join(book_path, "book.json")
             config = File.read(config_file)
             book_config = JSON.parse(config)
 
             book = Hash.new
-
             book["title"] = book_config["title"]
             book["end"] = book_config["end"]
 
@@ -46,6 +81,52 @@ module Jekyll
 
             book["slug"] = book_dir
             books << book
+
+            # TODO：创建书籍页面
+            summary = File.read(File.join(book_path, "SUMMARY.md")) # 怎么解析它应该是最大的难点
+
+            parts = self.parse_summary(summary)
+
+            chapters = []
+
+            # 生成章节页面
+            parts.each do |part|
+              chapter = ChapterPage.new(site, site.source, book_dir, part["link"], book, part)
+              chapters.push(chapter)
+            end
+
+            # 生成summary
+            items = []
+            current_item = nil
+            chapters.each do |chapter|
+              item = Hash.new
+              item["title"] = chapter.data["title"]
+              item["link"] = chapter.data["link"]
+              if chapter.data["level"] == 1
+                items.push(item)
+                current_item = item
+                item["chapters"] = []
+              else
+                current_item["chapters"].push(item)
+              end
+            end
+
+            puts items
+
+            book["parts"] = items
+
+            # 给章节设定上一页及下一页
+            chapters.each_with_index do |chapter, index|
+              if index > 0
+                chapter.data["prev"] = chapters[index - 1]
+              end
+              if index < chapters.size - 1
+                chapter.data["next"] = chapters[index]
+              end
+              site.pages << chapter
+            end
+
+
           end
         end
 
@@ -72,6 +153,39 @@ module Jekyll
       rescue Exception => e
         puts e
       end 
+    end
+
+    def parse_summary(summary)
+      require "nokogiri"
+
+      html = Kramdown::Document.new(summary).to_html
+
+      parsed_html = Nokogiri::HTML(html)
+
+      chapters = []
+
+      list = parsed_html.xpath("//body/ul/li")
+
+      list.each do |li|
+        chapter = Hash.new
+
+        chapter["link"] = li.xpath("a/@href").to_s
+        chapter["title"] = li.xpath("a/text()").to_s
+        chapter["level"] = 1
+        chapters.push(chapter)
+
+        li.xpath("ul/li").each do |sub_li|
+          sub_chapter = Hash.new
+
+          sub_chapter["link"] = sub_li.xpath("a/@href").to_s
+          sub_chapter["title"] = sub_li.xpath("a/text()").to_s
+          sub_chapter["level"] = 2
+
+          chapters.push(sub_chapter)
+        end
+
+      end
+      return chapters
     end
   end
 end
