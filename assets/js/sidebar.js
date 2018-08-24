806cc8b1f0a4ba29199fd895d9c8f146e7b12b01
zhangshiyu <741448394@qq.com>
2018-08-24 12:51:17 +0800

    修改sidebar.js

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -1,18 +1,23 @@
-var menuToggler = document.getElementById("sidebarToggler"),
-    sidebar = document.getElementById("sidebar"),
-    logo = document.getElementById("logo"),
-    isOpen = false;
+function bindSidebarEvent () {
+  var menuToggler = document.getElementById("sidebarToggler"),
+      sidebar = document.getElementById("sidebar"),
+      logo = document.getElementById("logo"),
+      isOpen = false;
 
-sidebarToggler.onclick = function() {
-  if( isOpen ) {
-    sidebar.classList.remove("active");
-    menuToggler.classList.remove("active");
-    logo.classList.remove("active");
-    isOpen = false;
-  } else {
-    sidebar.classList.add("active");
-    menuToggler.classList.add("active");
-    logo.classList.add("active");
-    isOpen = true; 
+  sidebarToggler.onclick = function() {
+    if( isOpen ) {
+      sidebar.classList.remove("active");
+      menuToggler.classList.remove("active");
+      logo.classList.remove("active");
+      isOpen = false;
+    } else {
+      sidebar.classList.add("active");
+      menuToggler.classList.add("active");
+      logo.classList.add("active");
+      isOpen = true; 
+    }
   }
 }
+document.addEventListener("turbolinks:load", function () {
+  bindSidebarEvent();
+});
