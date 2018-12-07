6ed217720363b14ea321cb9fef6710caeba66477
zhangshiyu <741448394@qq.com>
2018-12-07 17:35:48 +0800

    根据yml判断是否加载katex

--- _plugins/jekyll-katex-filter.rb
+++ _plugins/jekyll-katex-filter.rb
@@ -3,12 +3,16 @@ require 'execjs'
 module Jekyll
   module KatexFilter
     def katex(html)
-      site = @context.registers[:site].config
-      katex_js_path = File.join(site["source"], "src/katex.min.js")
+      is_latex = @context.registers[:page]["latex"]
 
-      katex_js = ExecJS.compile(File.open(katex_js_path).read)
-      parsed_html = html.to_s.gsub(/(?<!\\)([$]{1})(.+?)(?<!\\)\1/) do |match|
-        katex_js.call('katex.renderToString', Regexp.last_match(2), { displayMode: false })
+      if is_latex
+        site = @context.registers[:site].config
+        katex_js_path = File.join(site["source"], "src/katex.min.js")
+
+        katex_js = ExecJS.compile(File.open(katex_js_path).read)
+        parsed_html = html.to_s.gsub(/(?<!\\)([$]{1})(.+?)(?<!\\)\1/) do |match|
+          katex_js.call('katex.renderToString', Regexp.last_match(2), { displayMode: false })
+        end
       end
     end
   end
