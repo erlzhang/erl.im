cbb5abaa6bf81fda02a45fdbd3af3a845826582e
zhangshiyu <741448394@qq.com>
2018-12-28 14:14:43 +0800

    modify build command

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-bundle exec jekyll build
+bundle exec jekyll build --incremental
 
 uglifycss _site/assets/main.css > _site/assets/style.css
 mv _site/assets/style.css _site/assets/main.css
