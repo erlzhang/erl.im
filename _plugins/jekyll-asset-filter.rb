735848d161bcc7aa260a2d94884d1a8a988e5b46
zhangshiyu <741448394@qq.com>
2019-03-21 15:50:59 +0800

    make the url of images configed

--- _plugins/jekyll-asset-filter.rb
+++ _plugins/jekyll-asset-filter.rb
@@ -1,8 +1,27 @@
 module Jekyll
   module AssetFilter
+    require 'jekyll'
+    require 'nokogiri'
+
     def asset_version(input)
       site = @context.registers[:site].config
       "#{input}?v=#{site["version"]}"
+    end
+
+    def ext(content)
+      doc = Nokogiri::HTML.fragment(content)
+      return content unless doc
+
+      img_url = @context.registers[:site].config['img_prefix']
+
+      doc.css('img').each do |img|
+        original_url = img.get_attribute("src")
+        next if original_url =~ /\Ahttp/i
+
+        new_url = img_url + original_url
+        img.set_attribute('src', new_url)
+      end
+      doc.to_s
     end
   end
 end
