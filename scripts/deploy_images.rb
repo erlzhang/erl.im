d1e41e31a797fe17794d14e3126dc2aa2e2f0527
jk-sun <jk-sun@qq.com>
2020-04-11 17:21:14 +0800

    远程图片上传脚本

---
+++ scripts/deploy_images.rb
@@ -0,0 +1,3 @@
+require_relative './lib/oss/deploy_images.rb'
+
+DeployImages.new.bulk_push_object
---
+++ scripts/lib/oss/deploy_images.rb
@@ -0,0 +1,94 @@
+require 'aliyun/oss'
+require 'find'
+
+class DeployImages
+
+  def client
+    @client ||= Aliyun::OSS::Client.new(
+      endpoint: 'oss-cn-beijing.aliyuncs.com',
+      access_key_id: 'LTAI4Fm5UANWwexZufHWRXVH',
+      access_key_secret: '39KcROZbMPKniPquLhED34xaQNt2vR'
+    )
+  end
+
+  def buckets
+    @buckets ||= client.list_buckets
+  end
+
+  def bucket
+    @bucket ||= client.get_bucket('persephone')
+  end
+
+  # 文件列表
+  def objects
+    bucket.list_objects
+  end
+
+  # 远程文件列表
+  def remote_objects
+    objects.map(&:key)
+  end
+
+  # 本地文件列表
+  def local_objects
+    files = []
+    Find.find('img') { |path| files << path if File.file?(path) }
+    files.compact
+  end
+
+  # 上传到远程的文件列表
+  def object_diffs
+    local_objects - remote_objects
+  end
+
+  # 推送本地文件到远程
+  def push_object(path)
+    bucket.put_object(path, file: path)
+
+    check_push_status(path)
+  end
+
+  def check_push_status(path)
+    if remote_objects.include? path
+      upload_files[:S] << path
+      puts "文件上传成功：#{path}"
+      true
+    else
+      upload_files[:F] << path
+      puts "文件上传失败：#{path}"
+      false
+    end
+  end
+
+  def bulk_push_object(files = object_diffs)
+    upload_files_reload
+
+    files.each do |file_path|
+      push_object(file_path)
+    end
+
+    puts_upload_files
+  end
+
+  def upload_files
+    @upload_files ||= {
+      S: [],
+      F: []
+    }
+  end
+
+  private
+
+  def upload_files_reload
+    @upload_files = nil
+  end
+
+  def puts_upload_files
+    puts '========================================'
+    puts "本次上传结果：成功（#{upload_files[:S].count}），失败（#{upload_files[:F].count}）"
+    failed_files = upload_files[:F].join("\n")
+    puts "失败文件：\n #{failed_files}" if failed_files.size > 0
+    puts '========================================'
+  end
+
+end
