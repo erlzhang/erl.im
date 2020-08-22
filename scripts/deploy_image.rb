574b48b8b127c9879615eb72c2ef105da44e2ebf
erlzhang <zhangshiyu1992@hotmail.com>
2020-08-22 10:57:57 +0800

    删除无用文件

--- scripts/deploy_image.rb
+++
@@ -1,7 +0,0 @@
-require 'yaml'
-
-require_relative './lib/oss/deploy_image.rb'
-
-Setting = YAML.load(File.open('./local.yml'))
-
-DeployImages.new(Setting['oss']['id'], Setting['oss']['secret']).bulk_push_object
